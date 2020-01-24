a7484799a53d2ca2e8e426211841d13a5e907ec5
Freedom Evenden <ftevenden1@gmail.com>
2020-01-23 21:32:30 -0900

    refactored Summary into smaller components

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,28 +1,17 @@
 import React from 'react'
 import './Summary.css'
 import Total from './Total'
+import Features_summary from './Features_summary'
 
 class Summary extends React.Component {
   render() {
-    const summary = Object.keys(this.props.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.props.state.selected[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {this.props.currency.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-    
     return (
       <section className="main__summary">
       <h2>Your cart</h2>
-      {summary}
+      <Features_summary 
+        state={this.props.state}
+        currency={this.props.currency}
+      />
       <Total 
         state={this.props.state}
         currency={this.props.currency}
